149effd54c8f5cce9eea843212be7d3ebddffdf4
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2021-09-11 22:49:39 +0100

    Update packer bootstrap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,9 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+-- Bootstrap packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
 end
 
 vim.api.nvim_exec(
