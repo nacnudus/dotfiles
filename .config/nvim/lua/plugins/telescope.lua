eae19a7e4b5837000057cf21c909dff8f2a56465
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2021-09-11 23:12:59 +0100

    Reconfigure telescope mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,4 @@
 -- Telescope
-vim.api.nvim_set_keymap('n', '<leader>t', ':Telescope<CR>', {})
-vim.api.nvim_set_keymap('x', '<leader>t', ':Telescope<CR>', {})
-
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -13,3 +10,16 @@ require('telescope').setup {
     layout_strategy = 'flex',
   },
 }
+
+-- Leader mappings
+
+-- Open Telescope with a list of things it can do
+vim.api.nvim_set_keymap('n', '<leader>tt', [[<cmd>lua require('telescope.builtin').builtin()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<leader>tt', [[<cmd>lua require('telescope.builtin').builtin()<CR>]], { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>tb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tg', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tz', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t/', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>to', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
