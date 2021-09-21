0d07dc231dc2b2fe43ebb871aa576bea0dd54592
Duncan Garmonsway <nacnudus@gmail.com>
2021-09-22 00:56:11 +0100

    Sort vim plugins more sensibly

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,23 +1,29 @@
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
-  use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
-  use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-surround' -- Edit pairs of surrounding characters
-  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
-  use { 'nvim-telescope/telescope-symbols.nvim', requires = { { 'nvim-telescope/telescope.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+  -- Cheatsheet
+  use {
+    'sudormrfbin/cheatsheet.nvim',
+    requires = {
+      {'nvim-telescope/telescope.nvim'},
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'},
+    }
+  }
   -- Colour scheme with treesitter support. Check for better-maintained alternatives.
   use { "sainnhe/gruvbox-material" }
   -- Fancier statusline
   use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  -- Highlight, edit, and navigate code using a fast incremental parsing library
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  -- Additional textobjects for treesitter
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  -- Lists of things
+  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
+  use { 'nvim-telescope/telescope-symbols.nvim', requires = { { 'nvim-telescope/telescope.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+  -- Language server
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  -- Treesitter
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
   -- Auto-completion
   use {
     "hrsh7th/nvim-cmp",
@@ -33,23 +39,21 @@ require('packer').startup(function()
       "kdheepak/cmp-latex-symbols",
     }
   }
-  use {
-    'sudormrfbin/cheatsheet.nvim',
-    requires = {
-      {'nvim-telescope/telescope.nvim'},
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'},
-    }
-  }
   use 'onsails/lspkind-nvim' -- Add vscode-like pictograms to neovim built-in lsp
+  --  Tim Pope
+  use 'tpope/vim-fugitive' -- Git commands in nvim
+  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+  use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
+  use 'tpope/vim-surround' -- Edit pairs of surrounding characters
+  -- Languages
   use 'jalvesaq/nvim-r' -- R languauge plugin
 end)
 
 require('plugins.cheatsheet-nvim')
 require('plugins.gitsigns')
+require('plugins.lspkind-nvim')
 require('plugins.lualine-nvim')
+require('plugins.nvim-cmp')
+require('plugins.nvim-r')
 require('plugins.telescope')
 require('plugins.vim-commentary')
-require('plugins.nvim-cmp')
-require('plugins.lspkind-nvim')
-require('plugins.nvim-r')
