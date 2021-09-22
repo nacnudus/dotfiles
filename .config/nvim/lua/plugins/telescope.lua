ea00e05334c8193d354245c2e353144fbca52046
Duncan Garmonsway <nacnudus@gmail.com>
2021-09-22 01:53:24 +0100

    Use fzf syntax in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,6 +11,19 @@ require('telescope').setup {
   },
 }
 
+-- Use fzf for extended syntax such as exact matches
+-- https://github.com/junegunn/fzf#search-syntax
+-- | Token     | Match type                 | Description                          |
+-- | --------- | -------------------------- | ------------------------------------ |
+-- | `sbtrkt`  | fuzzy-match                | Items that match `sbtrkt`            |
+-- | `'wild`   | exact-match (quoted)       | Items that include `wild`            |
+-- | `^music`  | prefix-exact-match         | Items that start with `music`        |
+-- | `.mp3$`   | suffix-exact-match         | Items that end with `.mp3`           |
+-- | `!fire`   | inverse-exact-match        | Items that do not include `fire`     |
+-- | `!^music` | inverse-prefix-exact-match | Items that do not start with `music` |
+-- | `!.mp3$`  | inverse-suffix-exact-match | Items that do not end with `.mp3`    |
+require('telescope').load_extension('fzf')
+
 -- Leader mappings
 
 -- Open Telescope with a list of things it can do
