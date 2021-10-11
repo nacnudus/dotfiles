878de6c6c5ee2499cd13454b6b8039c2e699b50e
Duncan Garmonsway <nacnudus@gmail.com>
2021-10-11 22:42:12 +0100

    Add mappings to switch windows

--- .config/nvim/lua/native-config.lua
+++ .config/nvim/lua/native-config.lua
@@ -101,3 +101,13 @@ vim.opt.backupdir = '$HOME/.config/nvim/tmp/backup/'
 vim.opt.shadafile = '$HOME/.config/nvim/tmp/viminfo'
 vim.opt.undodir = '$HOME/.config/nvim/tmp/undo/'
 vim.opt.directory = '$HOME/.config/nvim/tmp/swap/'
+
+-- Switch between windows
+vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-n><C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-n><C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-n><C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-n><C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
