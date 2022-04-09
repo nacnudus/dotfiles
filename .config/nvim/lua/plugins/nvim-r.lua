3bc0d23e89e8aa7f80664699de1ca78405dfc034
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-04-09 22:15:46 +0100

    Tweak nvim-r for radian REPL

--- .config/nvim/lua/plugins/nvim-r.lua
+++ .config/nvim/lua/plugins/nvim-r.lua
@@ -5,7 +5,7 @@ vim.g.R_notmuxconf = 1  -- Use my own tmux, which allows mouse scroll
 -- Use Alt+- to create <- as in Rstudio
 vim.g.R_assign_map = "<M-->"
 
-vim.g.rout_follow_colorscheme = 1 -- Doesn't seem to work
+-- vim.g.rout_follow_colorscheme = 1 -- Doesn't seem to work
 vim.g.R_commented_lines  = 1 -- Include commented lines when sending to console
 vim.g.R_clear_line  = 1 -- Delete existing stuff on command line before sending command to R
 vim.g.R_rmd_environment = "new.env()" -- compile .Rmd in a fresh environment
@@ -31,18 +31,10 @@ vim.g.R_pdfviewer = "zathura"
 vim.g.R_insert_mode_cmds = 0 -- Don't use Nvim-R commands in insert mode
 vim.g.R_nvim_wd = 1 -- Use the Nvim working directory, not that of the file
 vim.g.r_syntax_folding = 1
-vim.g.Rout_more_colors = 1 -- highlight R commands in output as they are in scripts
+vim.g.Rout_more_colors = 0 -- don't highlight R commands in output as they are in scripts
 vim.g.rmd_fenced_languages = { 'r', 'python' }
 vim.g.markdown_fenced_languages = { 'r', 'python' }
 vim.g.R_non_r_compl = 0 -- use omni completion provided by another plugin
-
--- Reset the terminal width
-vim.api.nvim_set_keymap(
-  'n',
-  '<LocalLeader>sw',
-  ':call SendCmdToR("options(width = as.integer(Sys.getenv(\'COLUMNS\')))")<CR>',
-  { noremap = true, silent = true }
-)
 
 -- Glimpse an object
 vim.api.nvim_set_keymap('n', '<LocalLeader>gl', ':call RAction("dplyr::glimpse")<CR>', { noremap = true, silent = true })
