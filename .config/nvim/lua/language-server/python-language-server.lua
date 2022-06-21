2a3cc360173cc8d617b1fbc9edd549aa1f8dfb4c
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-06-21 20:32:50 +0100

    Explain need for python environment

--- .config/nvim/lua/language-server/python-language-server.lua
+++ .config/nvim/lua/language-server/python-language-server.lua
@@ -1,5 +1,8 @@
--- pip install jedi-language-server
-require'lspconfig'.jedi_language_server.setup{ 
+-- Install:
+-- python -m venv ~/.virtualenvs/neovim-python3
+-- source ~/.virtualenvs/neovim-python3/bin/activate
+-- python -m pip install jedi-language-server
+require'lspconfig'.jedi_language_server.setup{
 	on_attach = On_attach,
 	cmd = { "/home/nacnudus/.virtualenvs/neovim-python3/bin/jedi-language-server" }
 }
