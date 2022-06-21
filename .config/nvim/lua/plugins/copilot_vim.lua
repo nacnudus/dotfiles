284b6233a4050be6530a0e636ba186be01427c1d
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-06-21 21:02:50 +0100

    Remove GitHub copilot because it is no longer free

--- .config/nvim/lua/plugins/copilot_vim.lua
+++
@@ -1,8 +0,0 @@
--- Map Ctrl+J to copilot instead of Tab
-vim.api.nvim_set_keymap(
-  'i',
-  '<C-J>',
-  'copilot#Accept("<CR>")',
-  { silent = true, script = true, expr = true }
-)
-vim.g.copilot_no_tab_map = true
