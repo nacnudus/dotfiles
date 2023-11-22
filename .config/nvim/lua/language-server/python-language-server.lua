3b93f02f99ddc3406fea10fca685b87aac98c876
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-11-22 12:59:48 +0000

    Use system python jedi language server

--- .config/nvim/lua/language-server/python-language-server.lua
+++ .config/nvim/lua/language-server/python-language-server.lua
@@ -4,5 +4,5 @@
 -- python -m pip install jedi-language-server
 require'lspconfig'.jedi_language_server.setup{
 	on_attach = On_attach,
-	cmd = { "/home/nacnudus/.virtualenvs/neovim-python3/bin/jedi-language-server" }
+	cmd = { "/usr/bin/jedi-language-server" }
 }
