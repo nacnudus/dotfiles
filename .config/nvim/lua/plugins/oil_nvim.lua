97df18568f95072fee59821242aea785f4c73ca8
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-01-17 13:35:13 +0000

    Fix fugitive/oil incompatibility

--- .config/nvim/lua/plugins/oil_nvim.lua
+++ .config/nvim/lua/plugins/oil_nvim.lua
@@ -1,1 +1,12 @@
 require("oil").setup()
+
+-- oil.nvim breaks :GBrowse from vim-fugitive and vim-rhubarb, so we define our
+-- own :Browse command as it suggests.
+-- https://vi.stackexchange.com/a/43348
+vim.api.nvim_create_user_command(
+  'Browse',
+  function (opts)
+    vim.fn.system { 'xdg-open', opts.fargs[1] }
+  end,
+  { nargs = 1 }
+)
