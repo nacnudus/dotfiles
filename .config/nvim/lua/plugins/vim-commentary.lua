89519fe2db6000919072803eac090b3c45bc4e49
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-02-16 09:13:24 +0000

    feat: set commentstring for SQL

--- .config/nvim/lua/plugins/vim-commentary.lua
+++ .config/nvim/lua/plugins/vim-commentary.lua
@@ -1,2 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader>c', '<Plug>CommentaryLine', {})
 vim.api.nvim_set_keymap('x', '<leader>c', '<Plug>Commentary', {})
+
+-- Comment string for sql
+vim.cmd('autocmd FileType sql setlocal commentstring=--\\ %s')
