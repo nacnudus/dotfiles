405d32e0e1130bbc7ba59004c7564b727a2186c9
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-11-28 16:58:33 +0000

    chore: update oil.nvim

--- .config/nvim/lua/plugins/oil_nvim.lua
+++ .config/nvim/lua/plugins/oil_nvim.lua
@@ -6,7 +6,7 @@ require("oil").setup()
 vim.api.nvim_create_user_command(
   'Browse',
   function (opts)
-    vim.fn.system { 'xdg-open', opts.fargs[1] }
+    vim.fn.system { 'open', opts.fargs[1] }
   end,
   { nargs = 1 }
 )
